aed43a8fc3a6969d6dd4f64655dacebe7ce39ab9
MrLumi <lumilesff@hotmail.com>
2018-02-26 15:48:33 +0000

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,9 +1,17 @@
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'locales/pt.lua',
 	'config.lua',
 	'server.lua'
 }
 client_script {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'locales/pt.lua',
 	'config.lua',
 	'client.lua'
 }
